d71c7665b6fde563e6798a71cd23626c406b1260
Francinum <5572280+francinum@users.noreply.github.com>
2021-01-19 21:51:25 -0500

    APM Support

--- cloverfield/__init__.py
+++ cloverfield/__init__.py
@@ -1,1 +1,7 @@
 from cloverfield.app import app
+
+if __name__ == '__main__':
+	# Create a logging handler and attach it.
+	handler = LoggingHandler(client=apm.client)
+	handler.setLevel(logging.WARN)
+	app.logger.addHandler(handler)
