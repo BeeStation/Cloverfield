31a6a4027756097d7093ee88d7889391fa30f70d
Gallyus <5572280+francinum@users.noreply.github.com>
2021-03-07 20:45:06 -0500

    Returns returns returns ya bolt.

--- cloverfield/blueprints/vpn_detection.py
+++ cloverfield/blueprints/vpn_detection.py
@@ -8,9 +8,9 @@ vpn_detection = Blueprint('vpn_detection', __name__)
 @vpn_detection.route('/vpncheck')
 def vpn_processor():
     check_allowed(True)
-    jsonify({"message":"API route incomplete. Complain to Francinum."})
+    return jsonify({"message":"API route incomplete. Complain to Francinum."})
 
 @vpn_detection.route("/vpncheck-whitelist/<string:mode>")
 def vpn_whitelist(mode: str): #modes supported: `add` `remove`
     check_allowed(True)
-    jsonify({"message":"API route incomplete. Complain to Francinum."}), 501
+    return jsonify({"message":"API route incomplete. Complain to Francinum."}), 501
